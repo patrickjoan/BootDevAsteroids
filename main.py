aa8bffd9e82b84ad0b99520bbe7f64e5c7662665
Patrick Joan Thomson <39674144+patrickjoan@users.noreply.github.com>
2025-03-17 14:05:48 +0200

    Add and draw Player

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+from player import Player
 
 
 def main():
@@ -10,12 +11,15 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
 
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
         screen.fill(color="black")
+        player.draw(screen)
         pygame.display.flip()
 
         # Limit framerate to 60
