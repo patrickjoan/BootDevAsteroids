3170b13240a4f9f46caf9022971c679914b78701
Patrick Joan Thomson <39674144+patrickjoan@users.noreply.github.com>
2025-03-17 13:29:37 +0200

    Add framerate limiter

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
 import pygame
-from constants import *
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 
 
 def main():
     print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
 
     pygame.init()
-
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         for event in pygame.event.get():
@@ -18,6 +17,9 @@ def main():
 
         screen.fill(color="black")
         pygame.display.flip()
+
+        # Limit framerate to 60
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
